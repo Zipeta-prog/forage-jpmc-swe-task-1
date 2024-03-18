163bed1347948cf9324d5f8c4eda6d743df925da
Zipeta-prog <zipeta81@gmail.com>
2024-03-18 15:57:34 +0300

    getRatio method changes

--- client3.py
+++ client3.py
@@ -42,7 +42,11 @@ def getDataPoint(quote):
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
+    if price_b == 0:
+        return None 
+    else:
+        return price_a / price_b
+
 
 
 # Main
